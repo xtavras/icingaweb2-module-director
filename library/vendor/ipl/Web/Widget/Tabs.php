a2a982b06cf6f03523e65e4be933866339d641dc
Thomas Gelf <thomas@gelf.net>
2018-06-15 01:28:30 +0200

    Tabs: wrap add() exception

--- library/vendor/ipl/Web/Widget/Tabs.php
+++ library/vendor/ipl/Web/Widget/Tabs.php
@@ -2,9 +2,26 @@
 
 namespace dipl\Web\Widget;
 
-use Icinga\Web\Widget\Tabs as WebTabs;
 use dipl\Html\ValidHtml;
+use Icinga\Exception\ProgrammingError;
+use Icinga\Web\Widget\Tabs as WebTabs;
+use RuntimeException;
 
 class Tabs extends WebTabs implements ValidHtml
 {
+    /**
+     * @param string $name
+     * @param array|\Icinga\Web\Widget\Tab $tab
+     * @return $this
+     */
+    public function add($name, $tab)
+    {
+        try {
+            parent::add($name, $tab);
+        } catch (ProgrammingError $e) {
+            throw new RuntimeException($e->getMessage(), 0, $e);
+        }
+
+        return $this;
+    }
 }
